bf4ef2987f39c752d61fbed58faeb46ddce52978
Rubik <v.terekhin@g.nsu.ru>
2020-11-14 22:10:53 +0700

    mapmaker update commit

--- Lab3/mapmaker.cpp
+++ Lab3/mapmaker.cpp
@@ -1,11 +1,25 @@
 #include "mapmaker.h"
 #include "ui_mapmaker.h"
+#include <QGraphicsView>
+#include <QPen>
+#include <QBrush>
+
 
 mapMaker::mapMaker(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::mapMaker)
 {
     ui->setupUi(this);
+    QBrush brush;
+    this->setFixedSize (400, 320);
+    QGraphicsView *emptyMap = new QGraphicsView(this);
+    QGraphicsScene *map = new QGraphicsScene(this);
+    map->setSceneRect(0 , 0, 301, 301);
+    emptyMap->setScene(map);
+    for (int i = 0; i < 10; i++)
+        for(int j = 0; j < 10; j++)
+            map->addRect(i * 30, j * 30, 30, 30);
+
 }
 
 mapMaker::~mapMaker()
